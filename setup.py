dc0a969579adc29ab8f6edb87d562ff559927620
Adam Rogacewicz <adam.rogacewicz@gmail.com>
2022-02-28 20:54:30 +0100

    simple CLI in python

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='supercli',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'click'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'supercli=supercli:supercli'
+        ]
+    }
+)
---
+++ supercli.py
@@ -0,0 +1,48 @@
+import tempfile
+import click, zipfile, pathlib
+import os
+import tempfile
+from datetime import date
+
+@click.command()
+@click.option('--zipname', '-zn', help="Zip file name", required=True, prompt="Zip file name please")
+@click.option('--data', '-d', help="Example data", required=True, prompt="Example text for VERSION.txt file please")
+@click.option('--value', '-v', help="Update the date in updated.txt file", required=True, prompt="Updated the date? Yes - 1, No - 2")
+def supercli(zipname, data, value):
+    path = str(pathlib.Path().resolve()) + '\\' + zipname + ".zip"
+    try:
+        zipfile.ZipFile(path, 'r')
+    except FileNotFoundError:
+        print("Wrong file name.")
+        return
+    except zipfile.BadZipFile:
+        print("The file is corrupted")
+        return
+    
+    tmpfd, tmpname = tempfile.mkstemp(dir=os.path.dirname(zipname))
+    os.close(tmpfd)
+
+    zipname = zipname + '.zip'
+    with zipfile.ZipFile(zipname, 'r') as zin:
+         with zipfile.ZipFile(tmpname, 'w') as zout:
+             for item in zin.infolist():
+                 if str(value) != '1' and item.filename != 'VERSION.txt':
+                     zout.writestr(item, zin.read('updated.txt'))
+
+    os.remove(zipname)
+    os.rename(tmpname, zipname)
+
+    with zipfile.ZipFile(zipname, mode='a', compression=zipfile.ZIP_DEFLATED) as zf:
+        files = 0
+        for file in zf.infolist():
+            files = files + 1
+        if files == 0:
+            zf.writestr('VERSION.txt', data)
+            if str(value) == '1':
+                zf.writestr('updated.txt', str(date.today()))
+        else:
+            if str(value) == '1':
+                zf.writestr('VERSION.txt', data)
+                zf.writestr('updated.txt', str(date.today()))
+            else:
+                zf.writestr('VERSION.txt', data)
